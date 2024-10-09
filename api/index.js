3e315c2f95d484e0df3fbfddf8b2c36931736ecd
Damien Erambert <damien@erambert.me>
2024-10-08 23:03:39 -0700

    blah

--- api/index.js
+++ api/index.js
@@ -1,66 +1,45 @@
-import Fastify from 'fastify'
+import Fastify from "fastify";
+import * as cheerio from "cheerio";
 
 const app = Fastify({
   logger: true,
-})
+});
 
-app.get('/', async (req, reply) => {
-  return reply.status(200).type('text/html').send(html)
-})
+app.get("/", async (req, reply) => {
+  return reply.status(200).type("text/html").send("OK");
+});
 
-export default async function handler(req, reply) {
-  await app.ready()
-  app.server.emit('request', req, reply)
+app.get("/avatar", async (req, reply) => {
+  const { handle } = req.query;
+
+  if (!handle) {
+    return reply.code(404).send("Not found");
+  }
+
+  const avatar = await grabImage(handle);
+
+  if (!avatar) {
+    return reply.code(404).send("Not found");
+  }
+
+  const avatarUrl = `${avatar}?dpr=2&width=80&height=80&fit=cover&auto=webp`;
+
+  return reply.redirect(avatarUrl);
+});
+
+async function grabImage(handle) {
+  const page = await fetch(`https://cohost.org/${handle}`);
+  try {
+    const html = await page.text();
+    const $ = cheerio.load(html);
+
+    return $('meta[property="og:image"]').attr("content");
+  } catch (e) {
+    return undefined;
+  }
 }
 
-const html = `
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link
-      rel="stylesheet"
-      href="https://cdn.jsdelivr.net/npm/@exampledev/new.css@1.1.2/new.min.css"
-    />
-    <title>Vercel + Fastify Hello World</title>
-    <meta
-      name="description"
-      content="This is a starter template for Vercel + Fastify."
-    />
-  </head>
-  <body>
-    <h1>Vercel + Fastify Hello World</h1>
-    <p>
-      This is a starter template for Vercel + Fastify. Requests are
-      rewritten from <code>/*</code> to <code>/api/*</code>, which runs
-      as a Vercel Function.
-    </p>
-    <p>
-        For example, here is the boilerplate code for this route:
-    </p>
-    <pre>
-<code>import Fastify from 'fastify'
-
-const app = Fastify({
-  logger: true,
-})
-
-app.get('/', async (req, res) => {
-  return res.status(200).type('text/html').send(html)
-})
-
-export default async function handler(req: any, res: any) {
-  await app.ready()
-  app.server.emit('request', req, res)
-}</code>
-    </pre>
-    <p>
-    <p>
-      <a href="https://vercel.com/templates/other/fastify-serverless-function">
-      Deploy your own
-      </a>
-      to get started.
-  </body>
-</html>
-`
+export default async function handler(req, reply) {
+  await app.ready();
+  app.server.emit("request", req, reply);
+}
