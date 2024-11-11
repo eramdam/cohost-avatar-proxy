9106c6b82e01535af23524802a22928a7163f116
Damien Erambert <damien@erambert.me>
2024-11-10 23:16:02 -0800

    docker stuff

--- api/index.ts
+++ api/index.ts
@@ -53,6 +53,8 @@ app.all("*", (req, res) => {
   res.sendStatus(404);
 });
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server ready on port ${PORT}.`));
 
 export default app;
